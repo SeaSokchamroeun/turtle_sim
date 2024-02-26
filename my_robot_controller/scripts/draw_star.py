20995c5bd24c731c07ac2c74c4b9795388202c9e
Roeunsea <6023010027@camtech.edu.kh>
2024-02-26 08:05:00 -0800

    added Robot Control

--- my_robot_controller/scripts/draw_star.py
+++ my_robot_controller/scripts/draw_star.py
@@ -7,19 +7,17 @@ from math import pow, atan2, sqrt
 
 class TurtleBot:
 
-    def init(self):
+    def __init__(self):
         # Creates a node with name 'turtlebot_controller' and make sure it is a
         # unique node (using anonymous=True).
         rospy.init_node('turtlebot_controller', anonymous=True)
 
         # Publisher which will publish to the topic '/turtle1/cmd_vel'.
-        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',
-                                                  Twist, queue_size=10)
+        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
         # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
         # when a message of type Pose is received.
-        self.pose_subscriber = rospy.Subscriber('/turtle1/pose',
-                                                Pose, self.update_pose)
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
 
         self.pose = Pose()
         self.rate = rospy.Rate(10)
@@ -90,7 +88,7 @@ class TurtleBot:
         # If we press control + C, the node will stop.
         
 
-if __name__ == 'main':
+if __name__ == '__main__':
     try:
         x = TurtleBot()
         x.move2goal(8.4,5.4)
